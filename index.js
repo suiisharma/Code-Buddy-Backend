489df11cb3fc9218d173c806e6c951e278891f2e
suiisharma <onlycppcode@gmail.com>
2023-09-11 00:45:32 +0530

    Changes in index.js

--- index.js
+++ index.js
@@ -42,5 +42,5 @@ const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port} 🔥`);
-    console.log(`Frontend at ${process.env.FRONTEND_URL}`);
+    console.log(`Frontend at ${process.env.FRONTEND_URL} 🚀`);
 });

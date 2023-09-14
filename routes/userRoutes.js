30c94719ac3fbb704da616d71c4b45df63cf789b
unknown <akhilsharma1421@gmail.com>
2023-09-15 00:56:16 +0530

    FIxed bug after merging

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from "express";
 import { changePass, forgotPassword, login, logout, resetPassword, setProfile, signup, validateUser } from "../controllers/auth.js";
 import IsAuthenticated from './../middleware/tokenService.js';
 import multer from 'multer'
+import firebase from "../firebase/config.js"    //important to initialise firebase
 
 const upload = multer({ storage: multer.memoryStorage() })
 const router = express.Router()
